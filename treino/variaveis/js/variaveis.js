567a7a6369d3a415a72878ea455a0029ad91062d
Carlos-Eduardo5Qs <carloseduardorm@outlook.com>
2022-05-30 14:45:12 -0300

    Adicionando novos exercícios

---
+++ treino/variaveis/js/variaveis.js
@@ -0,0 +1,22 @@
+let nome = 'João'; // Declarando variável.
+let none; //Podemos criar variáveis vazias.
+let nomeCliente = "Carimbo";// Podemos usar aspas duplas :)
+
+console.log(nomeCliente)
+console.log(nome,'nasceu em 1994');
+console.log('Em 2020',nome,'Conheceu Maria');
+console.log(nome,'casou-se com maria em 2012');
+console.log('Maria teve 1 filho com',nome,'em 2015');
+console.log('O filho de', nome, 'se chama Eduardo');
+console.log(none);
+
+// Não podemos criar váriaveis reservadas pelo Javascript.
+// Variáveis precisam ter nome significativos.
+// Não podemos começar nomes de variáveis com números.
+// Não pode conter espaços ou traços
+// Utilizamos camelCase, Exemplo: "let = nomeCompletoDoCliente 'Carlos Eduardo Ramos Mariano;'"
+// Case-sensitive, Ex: (let = nomeCompleto '...'), (let = nomecompleto '...')."
+// Letras maiúsculas e minúsculas fazem diferença
+// Não podemos redeclarar variáveis com let
+
+                // -----------!!!NÃO UTILIZE "VAR", UTILIZE LET!!!-----------
